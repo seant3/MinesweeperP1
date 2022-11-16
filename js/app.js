d77ec6c62e3c5758bd2a8a8da8581c0164be64c4
seant3 <116916697+seant3@users.noreply.github.com>
2022-11-16 11:33:28 -0500

    Score counts when spaces clicked

--- js/app.js
+++ js/app.js
@@ -2,34 +2,35 @@
 const rows = 4;
 const cols = 5;
 const grid = [
-    [0,0,1,0,0],
     [0,0,0,0,0],
+    [1,0,0,0,0],
+    [0,0,0,1,0],
     [0,1,0,0,0],
-    [0,0,1,0,1],
 ];
 
-let playerScore = 0;
-let bombCount = 0;
-let freeSpaceCount = 0;
+let display;
+let outcome;
+// let bombCount = 0;
+// let freeSpaceCount = 0;
 let board = document.getElementById('board');
 
+
 function createBoard() { // creates 50px block buttons in relation to the values of 'rows' and 'cols'
-    for (let row = 0; row < rows; row++) {
-        for(let col = 0; col < cols; col++) {
-            if (grid[row][col]===1) {
+    for (let i = 0; i < rows; i++) {
+        for(let j = 0; j < cols; j++) {
+            if (grid[i][j]===1) {
                 let bomb = document.createElement('button');
                 bomb.className = 'bomb';
-                bomb.style.top = row*50 + 'px';
-                bomb.style.left = col*50 + 'px';
+                bomb.style.top = i*50 + 'px';
+                bomb.style.left = j*50 + 'px';
                 board.appendChild(bomb);
-                bomb.addEventListener('click', playRound);
-                console.log(grid.cellIndex) // Click a square in the grid
+                bomb.addEventListener('click', playRound); // Click a square in the grid
                 // can we count the amount of bombs here to display?
             } else {
                 let freeSpace = document.createElement('button');
                 freeSpace.className = 'freeSpace';
-                freeSpace.style.top = row*50 + 'px';
-                freeSpace.style.left = col*50 + 'px';
+                freeSpace.style.top = i*50 + 'px';
+                freeSpace.style.left = j*50 + 'px';
                 board.appendChild(freeSpace);
                 freeSpace.addEventListener('click', playRound); // Click a square in the grid
                 // count the amount of freeSpace so we know when we win?
@@ -51,10 +52,26 @@ function playRound(e){
         alert ('Game over, try again!');
     } else {
         btnClickedEl.className = 'clicked';
-        btnClickedEl.innerHTML = `I'm Alive`;
-        console.dir(btnClickedEl)
+        btnClickedEl.innerHTML = 1;
+        // let countMines = 0;
+        // let rowIndex = grid[rows]
+        // let colIndex = grid[cols]
+        // for(i = 0; i < 3; i++) {
+        //     for(j = 0; j < 3; j++) {
+
+        //     }
+        // }
+        // console.dir(btnClickedEl)
     }
+    display.score += 1
+
     render()
+}
+
+const displayEls = {
+    score: document.querySelector('#p-score'),
+    mines_remaining: document.querySelector('#mines_num'),
+    spaces_remaining: document.querySelector('#spaces_num')
 }
 
 // Reset button to refresh the page and play again
@@ -65,11 +82,21 @@ init()
 // Set initial state of game
 function init() {
     createBoard();
+
+    display = {
+        score: 0,
+        mines_remaining: 0,
+        spaces_remaining: 0
+    };
+
+    outcome = null;
     render()
 }
 
 function render(){
-
+    displayEls.score.innerText = display.score;
+    displayEls.mines_remaining.innerText = display.mines_remaining;
+    displayEls.spaces_remaining.innerText = display.spaces_remaining;
 }
 // Use randomization so they move each time it is reset
 
