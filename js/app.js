b27aee7261d96e026a92b0c6b34dcd48511399e0
seant3 <116916697+seant3@users.noreply.github.com>
2022-11-17 09:50:16 -0500

    Cleaned up CSS

--- js/app.js
+++ js/app.js
@@ -10,10 +10,8 @@ const grid = [
 let freeSpace;
 let bomb;
 let display;
-let outcome;
-// let bombCount = 0;
-// let freeSpaceCount = 0;
-let board = document.getElementById('board');
+let outcome = '';
+// let board = document.getElementById('board');
 
 function createBoard() { // creates 50px block buttons in relation to the values of 'rows' and 'cols'
     for (let i = 0; i < rows; i++) {
@@ -26,68 +24,52 @@ function createBoard() { // creates 50px block buttons in relation to the values
                 board.appendChild(bomb);
                 bomb.addEventListener('click', playRound); // Click a square in the grid
                 display.minesRemaining += 1;
-
-                // can we count the amount of bombs here to display?
             } else {
                 freeSpace = document.createElement('button');
                 freeSpace.className = 'freeSpace';
                 freeSpace.style.top = i*50 + 'px';
                 freeSpace.style.left = j*50 + 'px';
                 board.appendChild(freeSpace);
-                freeSpace.addEventListener('click', playRound);
-                 // Click a square in the grid
+                freeSpace.addEventListener('click', playRound);  // Click a square in the grid
                 display.spacesRemaining += 1;
-                // count the amount of freeSpace so we know when we win?
-            // }
             }
         }
     }
 }
-
 
 function playRound(e){
     const btnClickedEl = e.target;
     if(btnClickedEl.className === 'bomb'){
         let showAll = document.querySelectorAll('.bomb');
         let endGame = document.querySelectorAll('.freeSpace')
+        
         endGame.forEach(function(els) {
             els.removeEventListener('click', playRound)
         })
+        
         showAll.forEach(function(el) {
             el.classList.add('blowup'); // If you hit a mine, show all the mine locations in a different color
             el.innerHTML = 'Bye Bye';
             el.removeEventListener('click', playRound)
            });
         
-        alert ('Game over, try again!');
-
-    
+        displayEls.outcome.innerHTML = "You went boom :("
+        return;
     } else {
         btnClickedEl.className = 'clicked';
-        btnClickedEl.innerHTML = 1;
         if (btnClickedEl.className = 'clicked') btnClickedEl.removeEventListener('click', playRound)
-
-        // let countMines = 0;
-        // let rowIndex = grid[rows]
-        // let colIndex = grid[cols]
-        // for(i = 0; i < 3; i++) {
-        //     for(j = 0; j < 3; j++) {
-
-        //     }
-        // }
-        // console.dir(btnClickedEl)
     }
-
+    
     display.score += 1
     display.spacesRemaining -= 1
     render()
-    
 }
 
 const displayEls = {
     score: document.querySelector('#p-score'),
-    minesRemaining: document.querySelector('#mines_num'),
-    spacesRemaining: document.querySelector('#spaces_num')
+    minesRemaining: document.querySelector('#mines-num'),
+    spacesRemaining: document.querySelector('#spaces-num'),
+    outcome: document.querySelector('#p-outcome')
 }
 
 // Reset button to refresh the page and play again
@@ -100,12 +82,11 @@ function init() {
     display = {
         score: 0,
         minesRemaining: 0,
-        spacesRemaining: 0
+        spacesRemaining: 0,
+        outcome: ''
     };
-
+    
     createBoard();
-
-    outcome = null;
     render()
 }
 
@@ -113,7 +94,14 @@ function render(){
     displayEls.score.innerText = display.score;
     displayEls.minesRemaining.innerText = display.minesRemaining;
     displayEls.spacesRemaining.innerText = display.spacesRemaining;
+
+    if(display.spacesRemaining === 0) {
+        displayEls.outcome.innerText = "You Win!";
+    }  else {
+      displayEls.outcome.innerText = "Watch out for that mine!";
+    } 
 }
+
 // Use randomization so they move each time it is reset
 
 // Display outcome - either a score or a message saying they were blown up
