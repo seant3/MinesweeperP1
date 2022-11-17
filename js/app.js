2cffd48038d3fb722caa684dcd93c86fbf9d22a0
seant3 <116916697+seant3@users.noreply.github.com>
2022-11-17 10:49:59 -0500

    Test

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ let bomb;
 let display;
 let outcome = '';
 let board = document.getElementById('board');
+console.log(Test)
 
 function createBoard() { // creates 50px block buttons in relation to the values of 'rows' and 'cols'
     for (let i = 0; i < rows; i++) {
